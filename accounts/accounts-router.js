91f602453e7a3b7c6827b1a1d4cdee8de0569fc7
Lambda_School_Loaner_264 <lambda_school_loaner_264@Lambda-School-Loaner-264s-MacBook-Pro.local>
2020-02-18 00:52:53 -0500

    Added Post Request
    MVP

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -4,6 +4,17 @@ const router = express.Router()
 
 const db = require('../data/dbConfig');
 
+router.post("/", (req, res) => {
+    db('accounts')
+    .insert(req.body, "id")
+    .then( account => {
+        res.status(201).json(account)
+    })
+    .catch(() => {
+        res.status(500).json({ message: 'Could not add account'})
+    })
+});
+
 router.get('/', (req, res) => {
     db('accounts')
     .then( accounts => {
@@ -30,7 +41,33 @@ router.get('/:id', (req, res) => {
     })
 })
 
-
+router.put('/:id', (req, res) => {
+    db('accounts')
+      .where({ id: req.params.id })
+      .update(req.body)
+      .then(change => {
+        if (change) {
+          res.status(200).json({ message: `${change} account(s) updated` });
+        } else {
+          res.status(404).json({ message: 'Account not found' });
+        }
+      })
+      .catch(() => {
+        res.status(500).json({ message: 'Could not update the account' });
+      });
+  });
+  
+  router.delete('/:id', (req, res) => {
+    db('accounts')
+      .where({ id: req.params.id })
+      .del()
+      .then(count => {
+        res.status(200).json({ message: `${count} record(s) deleted` });
+      })
+      .catch(() => {
+        res.status(500).json({ message: 'Could not remove the account' });
+      });
+  });
 
 
 
